e0894aca4cd6faa89eb80eaf7b60a83053fcf2b6
Jaydeepsolanki76 <jay.solanki7694@gmail.com>
2023-10-27 17:21:09 +0530

    added model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   rolify
+  after_create :assign_default_role
+  has_many :books, foreign_key: :author_id
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  after_create :assign_default_role
 
   def assign_default_role
     self.add_role(:user) if self.roles.blank?
