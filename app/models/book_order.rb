dd26cd14f1860c6843751725d54ffcfcd7e585b2
Jaydeepsolanki76 <jay.solanki7694@gmail.com>
2023-11-20 17:33:21 +0530

    added order resources in active admin

--- app/models/book_order.rb
+++ app/models/book_order.rb
@@ -1,4 +1,8 @@
 class BookOrder < ApplicationRecord
   belongs_to :book
   belongs_to :order, optional: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["book_id", "created_at", "id", "id_value", "order_id", "quantity", "updated_at"]
+  end
 end
